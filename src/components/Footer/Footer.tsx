aab590e679735b708be276548b40362824800acc
AKAGEGE87 <akagege87@gmail.com>
2025-11-23 19:30:10 +0400

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,69 @@
+const socLinks = [
+  { title: "/images/twitch.svg", url: "https://www.twitch.tv" },
+  { title: "/images/instagram.svg", url: "https://www.instagram.com" },
+  { title: "/images/twitter.svg", url: "https://www.twitter.com" },
+  { title: "/images/facebook.svg", url: "https://www.facebook.com" },
+  { title: "/images/youtube.svg", url: "https://www.youtube.com" }
+];
+
+const navLinks = [
+  { title: "CONTANO", url: "/" },
+  { title: "SOBRE", url: "/about" },
+  { title: "SUPORTE", url: "/contact" },
+  { title: "Empregos", url: "/blog" }
+];
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex justify-between items-center px-[140px] py-[91px] bg-[#020202]">
+      <div className="flex gap-10">
+        <div className="border-2 border-white w-[57.64px] h-[52.81px] rounded-2xl flex items-center justify-end pr-1.5">
+          <a href="/">
+            {" "}
+            <img
+              src="/images/Logo.svg"
+              alt="logo"
+              className="w-[34.83px] h-[35.28px]"
+            />
+          </a>
+        </div>
+        <div className="flex flex-col gap-2 text-[#797979]">
+          <nav>
+            <ul className="flex gap-6">
+              {navLinks.map((link) => (
+                <li key={link.title}>
+                  <a
+                    href={link.url}
+                    className="uppercase text-[16px] hover:text-white"
+                  >
+                    {link.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <p className="text-[13px]">
+            ©2023 Rockstar Games, Inc. - YWR Studio.{" "}
+          </p>
+        </div>
+      </div>
+      <nav>
+        <ul>
+          {socLinks.map((link, index) => (
+            <li key={index} className="inline-block mr-8">
+              <a href={link.url} target="_blanck">
+                <img
+                  src={link.title}
+                  alt="social link"
+                  className="hover:size-8"
+                />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Launcher/Launcher.tsx
+++ src/components/Launcher/Launcher.tsx
@@ -2,12 +2,12 @@ import Button from "../Button/Button";
 
 const Launcher = () => {
   return (
-    <section className="px-[140px] mt-28 ">
-      <div className="bg-cover bg-center bg-[url('/images/launcherBG.png')] h-[535px] flex gap-20 rounded-2xl pl-[76px] py-8">
+    <section className="px-[140px] mt-28 mb-[85px]">
+      <div className="bg-cover bg-center bg-[url('/images/launcherBG.png')] h-[535px] flex justify-between gap-20 rounded-2xl pl-[76px] py-8">
         <div>
           <div className="flex gap-5 justify-center items-center max-w-[342px] mt-8 mb-6">
             <div className="max-w-[126.75px] w-full max-h-[137.65px] h-screen bg-[#FFAB00] flex justify-center items-center rounded-2xl border-4 border-black">
-              <img src="/images/LogoBlack.svg" alt="logo" />
+              <img src="/images/LogoBlack.svg" alt="logo" className="" />
             </div>
 
             <h1 className="font-extrabold text-[41.75px] text-[#020202] ">
@@ -28,7 +28,7 @@ const Launcher = () => {
           <img
             src="/images/lastsection.png"
             alt="image"
-            className="w-full h-full object-cover object-center"
+            className="w-full h-full object-cover object-center "
           />
         </div>
       </div>
